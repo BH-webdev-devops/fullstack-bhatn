d14cfaabef47a0cd36359cebab5f1e04726ca8f1
ozyegenl <lara.ozyegen@bahag.com>
2024-11-18 11:43:56 +0100

    initial deploy

--- backend/db/db.ts
+++ backend/db/db.ts
@@ -4,11 +4,11 @@ import 'dotenv/config';
 
 // Configure the PostgreSQL connection pool
 export const pool = new Pool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: String(process.env.DB_PASSWORD),
-    database: process.env.DB_NAME,
-    port: Number(process.env.DB_PORT),
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: String(process.env.DATABASE_PASSWORD),
+    database: process.env.DATABASE_NAME,
+    port: Number(process.env.DATABASE_PORT),
   });
 
 // Generic query function to interact with PostgreSQL
